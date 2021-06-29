c2da686d85ea24c28a233343bcdaf4b7746eac07
Adetoye Babalola <adetoyebamise@gmail.com>
2021-06-29 22:08:48 +0100

    make main branch clean

--- server.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const app = express();
-const logger = require("./configs/logger");
-const port = 3000 || process.env.PORT;
-app.listen(3000, () => {
-  logger.log("info", `Alive on port ${port}`);
-});
