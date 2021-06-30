bee2a1c9c6fda214cf7879fffe35f94fe4c8cb07
olaifa olawale <olaifaolawaleh@gmail.com>
2021-06-30 13:27:24 +0100

    user registration

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const logger = require("./configs/logger");
 const port = 3000 || process.env.PORT;
+const route = require('./route/index')
+
+route(app)
 
 app.listen(3000, () => {
   logger.log("info", `Alive on port ${port}`);
--- service/auth/user.auth.js
+++ service/auth/user.auth.js
@@ -1,8 +1,17 @@
 const Users = require('../../model/user-model/userModel')
 require('express-async-errors')
+const bcrypt = require('bcrypt')
 
-module.exports = class {
+module.exports = class UserAuthService{
+    /**
+     * 
+     * @param {registration details} profile 
+     * @returns created user's profile after saving to the database
+     */
     static async userRegistration(profile) {
-
+        const newUser = await new Users(profile)
+        let salt = await bcrypt.genSalt(10)
+        newUser.password = await bcrypt.hash(profile.password, salt)
+        return await newUser.save()
     }
 }
