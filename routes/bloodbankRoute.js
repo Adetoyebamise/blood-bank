2e7f872f174f667e84e753f0a99ebc76555fd63d
Oluwasegun Ajayi <ajayimooseize@gmail.com>
2021-06-30 16:08:50 +0100

    Set up routes for authentication

--- routes/bloodbankRoute.js
+++ routes/bloodbankRoute.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const router = express.Router();
-//const { registerUser } = require('../controllers/auth/user.auth')
 
 router.get("/", (req, res) => {
   res.send("Other routes here");
