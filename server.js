78167a4279f6caed14345b67165736d32f237cf0
Adetoye Babalola <adetoyebamise@gmail.com>
2021-07-02 06:01:08 +0100

    resolve conflict on server file

--- server.js
+++ server.js
@@ -2,16 +2,16 @@ const express = require("express");
 const app = express();
 // const logger = require("./configs/logger");
 const port = 3000 || process.env.PORT;
-const route = require('./routes/index')
-const connectDB = require('./dbConnection')
-const middleware = require('./middlewares/index')
+const route = require("./routes/index");
+const connectDB = require("./dbConnection");
+const middleware = require("./middlewares/index");
 
-middleware(app)
-route(app)
+middleware(app);
+route(app);
+
+// set up app middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 //connect to db
 connectDB(app);
-
-app.listen(port, () => {
-  console.log("info", `Alive on port ${port}`);
-});
