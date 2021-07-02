166bde363bd03c3ec9d5bea34a9af19bbab32764
Adetoye Babalola <adetoyebamise@gmail.com>
2021-07-02 07:01:27 +0100

    comment code out for integratin test

--- models/bloodbank-model/bloodbankModel.js
+++ models/bloodbank-model/bloodbankModel.js
@@ -1,30 +1,29 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
+// const mongoose = require("mongoose");
+// const Schema = mongoose.Schema;
+// const bloodbankSchema = new Schema(
+//   {
+//     bloodbankName: {
+//       type: String,
+//       required: true,
+//     },
+//     bloodbankType: {
+//       type: String,
+//       enum: ["public", "private"],
+//       required: true,
+//     },
+//     email: {
+//       type: String,
+//       required: true,
+//     },
+//     password: {
+//       type: String,
+//       required: true,
+//     },
+//   },
+//   { timestamps: true }
+// );
 
-const bloodbankSchema = new Schema(
-  {
-    bloodbankName: {
-      type: String,
-      required: true,
-    },
-    bloodbankType: {
-      type: String,
-      enum: ["public", "private"],
-      required: true,
-    },
-    email: {
-      type: String,
-      required: true,
-    },
-    password: {
-      type: String,
-      required: true,
-    },
-  },
-  { timestamps: true }
-);
-
-module.exports = mongoose.model("Bloodbank", bloodbankSchema);
+// module.exports = mongoose.model("Bloodbank", bloodbankSchema);
 const mongoose = require("mongoose"),
   bcrypt = require("bcrypt"),
   bloodBankSchema = new mongoose.Schema(
