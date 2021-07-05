39584065c12b81e2030703a5bb514383cbda5210
Oluwasegun Ajayi <ajayimooseize@gmail.com>
2021-07-05 21:56:22 +0100

    implemented user route for search

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,4 +1,11 @@
 const express = require("express");
 const router = express.Router();
+//User controller
+const user = require("../controllers/user-controller/userController");
 
-router.get();
+// User can search for bloodbanks by name. Name would be in req.body. i.e search form
+router.post("/:userID/donate-blood", (req, res) => {
+  user.search(req, res);
+});
+
+module.exports = router;
