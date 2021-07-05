53142eb8504cfcb0efcfd4106dbe186f815fa1b8
Oluwasegun Ajayi <ajayimooseize@gmail.com>
2021-07-05 21:55:22 +0100

    user route

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,13 @@
 const userAuth = require("./user.auth");
+const userRoutes = require("./userRoute");
 const bloodbankAuth = require("./bloodbank.auth");
 const bloodbankRoutes = require("./bloodbankRoute");
 
 module.exports = (app) => {
   //user registration
   app.use("/api/v1/user/auth", userAuth);
+  //For Other User's route
+  app.use("/api/v1/user", userRoutes);
   //Other bloodbank routes - Apart from authentication
   app.use("/api/v1/bloodbank", bloodbankRoutes);
   //For bloodbank Authentication
