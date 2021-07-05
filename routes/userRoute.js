b10e9ab6517363a63c6d711f90ec59e44e1a13a7
vicky2k <vicspidin@gmail.com>
2021-07-05 23:33:50 +0100

    Last Commit for the Day

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -8,41 +8,25 @@ const authenticateUser = require("../controllers/auth/user.auth");
 
 const UserCtrl = require("../controllers/user-controller/userController");
 const donationCtrl = require("../controllers/user-controller/donationHistory");
-// router.get();
 
-/** This is a GET Request to Endpoint
-api/v1/user/user:id/profile/
-to get all users
- * */
-router.get("/api/v1/user/user:id/profile", UserCtrl.fetchSingleUser);
+router.post("/buyblood/:userId", (req, res) =>
+  UserController.buyBloodRequest(req, res)
+);
 
 /**
- * This is a PUT Request to Endpoint
- * api/v1/user/user:id/profile/update
- * to Update the profile of a user
+ *  Request to Various Endpoints
+ * api/v1/user/user:id/profile/
+ * to Update the profile of a user and
+ * Quering the DB for user Blood Donation history
  */
 
 router.get(
   "/api/v1/user/user:id/history/donationsummary",
   donationCtrl.getAllDonations
 );
-/**
- * This is a GET Request to Endpoint
- * api/v1/user/user:id/history/donationsummary
- * to implement Donation History
- */
+
+router.get("/api/v1/user/user:id/profile", UserCtrl.fetchSingleUser);
 router.put("/api/v1/user/user:id/profile/update", UserCtrl.updateSingleUser);
-
-/**
- * This is a DELETE Request to Endpoint
- * api/v1/user/user:id/profile/edit
- * to Edit the profile of a user
- */
-
 router.delete("/api/v1/user/user:id/profile/edit", UserCtrl.deleteSingleUser);
-
-router.post("/buyblood/:userId", (req, res) =>
-  UserController.buyBloodRequest(req, res)
-);
 
 module.exports = router;
