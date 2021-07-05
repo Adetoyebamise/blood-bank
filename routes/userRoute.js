c31df90546b7b25bac8f3eee4e59faedfffc1bba
vicky2k <vicspidin@gmail.com>
2021-07-05 17:53:46 +0100

    Get Request for Donation History

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -6,7 +6,7 @@ const authenticateUser = require("../controllers/auth/user.auth");
 // require("../middlewares/index");
 
 const UserCtrl = require("../controllers/user-controller/userController");
-
+const donationCtrl = require("../controllers/user-controller/donationHistory");
 // router.get();
 
 /** This is a GET Request to Endpoint
@@ -19,6 +19,16 @@ router.get("/api/v1/user/user:id/profile", UserCtrl.fetchSingleUser);
  * This is a PUT Request to Endpoint
  * api/v1/user/user:id/profile/update
  * to Update the profile of a user
+ */
+
+router.get(
+  "/api/v1/user/user:id/history/donationsummary",
+  donationCtrl.getAllDonations
+);
+/**
+ * This is a GET Request to Endpoint
+ * api/v1/user/user:id/history/donationsummary
+ * to implement Donation History
  */
 router.put("/api/v1/user/user:id/profile/update", UserCtrl.updateSingleUser);
 
